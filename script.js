b3f907e6715a2e1c858a6f780d3af93b98d51190
Okkevideka <148295938+Okkevideka@users.noreply.github.com>
2024-02-26 22:30:14 -0500

    javascript prompt
    testing variables.

---
+++ script.js
@@ -0,0 +1,7 @@
+var hello = ("Hello and welcome to my first JavaScript variable!");
+alert(hello);
+var hello1 = ("I'm really just testing out how variables work and seeing if my brain can progress all of this!");
+alert(hello1);
+var hello2 = prompt("What is your name by any chance?");
+alert("Wow Hello " + hello2 +" Nice to meet you!");
+alert("Well it was nice meeting you " + hello2 + " Have a great day!");
